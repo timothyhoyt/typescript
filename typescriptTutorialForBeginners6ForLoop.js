bfb67d880472dfd1f6a7a850eeb60ec95f24a5fa
timothyhoyt <136997526+timothyhoyt@users.noreply.github.com>
2023-08-01 10:38:36 -0500

    youtubeDemos

---
+++ typescriptTutorialForBeginners6ForLoop.js
@@ -0,0 +1,4 @@
+var num = 3;
+for (var i = 1; i < 11; i++) {
+    console.log(num + "x" + i + " = " + num * i);
+}
---
+++ typescriptTutorialForBeginners6ForLoop.ts
@@ -0,0 +1,5 @@
+let num: number=3;
+
+for(let i:number=1; i<11; i++){
+    console.log(num + "x" + i + " = " + num * i);
+}
